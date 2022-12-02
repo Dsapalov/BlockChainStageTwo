db1f91db5d0ff916b6e05441e48f214c48bfa020
Dsapalov <denis.sapalov@gmail.com>
2022-12-02 05:33:11 +0200

    Initial project setup

---
+++ BabyBlockChain/BabyBlockChain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BabyBlockChain
+//
+//  Created by Denis Sapalov on 02.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# BlockChainStageOne
+# BlockChainStageTwo
 
 Non-finished - urgent electicity problem, I guess it will be finished in nearest 2 weeks(curr time 24 Nov 2022) 
